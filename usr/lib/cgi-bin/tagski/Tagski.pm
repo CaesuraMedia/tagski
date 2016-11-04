579ac9ef3b449504285515a7e57bed920e75b03a
Andy Cragg <andyc@caesura.co.uk>
2016-11-04 13:16:51 +0000

    Fixed error in Tagski.pm while removing pw

--- usr/lib/cgi-bin/tagski/Tagski.pm
+++ usr/lib/cgi-bin/tagski/Tagski.pm
@@ -5,5 +5,6 @@
 use strict;
 package Tagski::DBI;
 use base 'Class::DBI';
-Tagski::DBI->connection('dbi:mysql:tagski:localhost:3306', 'root', 'not_my_password')1;
+Tagski::DBI->connection('dbi:mysql:tagski:localhost:3306', 'root', 'not_my_password');
+1;
 
